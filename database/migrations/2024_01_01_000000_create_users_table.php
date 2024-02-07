f86669cc3f0eaa31e5be4a97e6199feb41c81bad
AustinPardosi <gabrielpardosi26@gmail.com>
2024-02-07 14:17:40 +0700

    feat: setup database

---
+++ database/migrations/2024_01_01_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('full_name');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('company_name')->nullable();
+            $table->string('company_address')->nullable();
+            $table->string('company_phone')->nullable();
+            $table->string('company_logo_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
