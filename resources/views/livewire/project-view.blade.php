d2fef93096449fbcea1e3cd8b661d461bbad1324
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-02-11 17:30:09 +0700

    add icons to project view

--- resources/views/livewire/project-view.blade.php
+++ resources/views/livewire/project-view.blade.php
@@ -16,9 +16,15 @@
                 <li>
                     <div wire:key="{{$p->project_id}}" class="project-list">
                         <h2 style="margin:auto; color: green;">{{ $p->project_name }}</h2>
-                        <p>{{ $p->created_at }}</p>
+                        <div>
+                            <img src="{{ asset('clock icon.png') }}" style="color: grey; width:15px; height:15px; float:left; margin-right:5px;" />
+                            <p style="color: grey;">{{ $p->created_at }}</p>
+                        </div>
                         <p style="font-size:18px;">{{ $p->client_name }} : Rp. {{ $p->budget }}</p>
-                        <p>{{ $p->project_address }}</p>
+                        <div style="vertical-align: middle;">
+                            <img src="{{ asset('gps icon.png') }}" style="color: grey; width:16px; height:20px; float:left; margin-right:5px;" />
+                            <p style="color: grey;">{{ $p->project_address }}</p>
+                        </div>
                         <div style="text-align:right;">
                             <button wire:click="seeRab({{ $p->project_id }}, '{{ $p->project_name }}')" style="background-color:green; color:white; padding: 10px; border:none;">Lihat Selengkapnya</button>
                         </div>
