26b7e5474df916b6754c3891a2e792e21d3cf6d3
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-02-15 15:05:23 +0700

    feat: models

---
+++ app/Models/Project.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'project_id';
+    public $fillable = [
+            'user_id',
+            'client_name',
+            'project_address',
+            'project_name',
+            'budget',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function rab()
+    {
+        return $this->hasMany(Rab::class);
+    }
+}
