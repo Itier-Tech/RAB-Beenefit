d75f1dfd01240a28a859a61686e798043e1d758b
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-02-25 12:20:55 +0700

    responsive rab page

--- resources/views/livewire/rab-page.blade.php
+++ resources/views/livewire/rab-page.blade.php
@@ -1,10 +1,26 @@
 <div style="width:80%; margin: auto;">
     <style>
+        @media (max-width: 768px) {
+            .table-container {
+                overflow-x:scroll;
+                /* Hide scrollbar for IE, Edge and Firefox */
+                -ms-overflow-style: none;
+                scrollbar-width: none; 
+            }
+            /* Hide scrollbar for Chrome, Safari and Opera */
+            .table-container::-webkit-scrollbar {
+                display: none;
+            }
+        }
+        .table-container {
+            width:100%;
+        }
         table {
             width: 100%; 
             margin: 3vh auto; 
             border-collapse: separate;
             border-spacing: 0 1em;
+            text-align:center;
         }
         tr {
             text-align: center; 
@@ -17,6 +33,12 @@
         th, td {
             padding: 0.5rem;
         }
+        .long-col {
+            min-width:15rem;
+        }
+        .short-col {
+            min-width:2rem;
+        }
         td {
             border-top: 1px solid black;
             border-bottom: 1px solid black;
@@ -24,28 +46,29 @@
     </style>
     <h2 style="margin: 5vh 0 0 0; font-weight:bold;">RAB {{ $project_name }}</h2>
     <button class="btn" wire:click='addRab' style="background-color: #FFD700; padding: 0.5rem 0.8rem; border:none; margin: 3vh 0 0 0;">Buat RAB Baru +</button>
-    <a href="#" style="float:right; margin: 0; color:green;">Lihat detail</a>
-    <table>
-        <tr style="background-color:#FFD700;">
-            <th>No</th>
-            <th>Tanggal</th>
-            <th>Total Harga Beli (Rp.)</th>
-            <th>Total Harga Jual (Rp.)</th>
-            <th>Aksi</th>
-        </tr>
-        @foreach ($rabList as $rab)
-            <tr wire:key="{{ $rab->rab_id }}" style="">
-                <td>{{ $count++ }}</td>
-                <td>{{ $rab->created_at }} WIB</td>
-                <td>{{ number_format($rab->total_price , 0, ',', '.') }}</td>
-                <td>{{ number_format($rab->total_price , 0, ',', '.') }}</td>
-                <td>
-                    <button class="btn" style="background-color:#FFA07A; border-radius:25px; padding: 0.1rem 0.6rem; margin-right: 0.3rem;">Lihat Detail</button>
-                    <button class="btn" wire:click="deleteRab({{ $rab->rab_id }})" style="background:none;"><img src="{{ asset('images/trash-icon.svg') }}"></button>
-                </td>
+    <div class="table-container">
+        <table>
+            <tr style="background-color:#FFD700;">
+                <th class="short-col">No</th>
+                <th class="long-col">Tanggal</th>
+                <th class="long-col">Total Harga Beli (Rp.)</th>
+                <th class="long-col">Total Harga Jual (Rp.)</th>
+                <th class="long-col">Aksi</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($rabList as $rab)
+                <tr wire:key="{{ $rab->rab_id }}" style="">
+                    <td>{{ $count++ }}</td>
+                    <td>{{ $rab->created_at->format('d-m-Y H:i:s') }} WIB</td>
+                    <td>{{ number_format($rab->total_price , 0, ',', '.') }}</td>
+                    <td>{{ number_format($rab->total_price , 0, ',', '.') }}</td>
+                    <td>
+                        <button class="btn" style="background-color:#FFA07A; border-radius:25px; padding: 0.1rem 0.6rem; margin-right: 0.3rem;">Lihat Detail</button>
+                        <button class="btn" wire:click="deleteRab({{ $rab->rab_id }})" style="background:none;"><img src="{{ asset('images/trash-icon.svg') }}"></button>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
     <div style="width: 60%; margin:auto;">
         {{ $rabList->links('project-pagination-links') }}
     </div>
