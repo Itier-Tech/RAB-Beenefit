858746bc6493fb92af4ab48283a7ebe7aefe7ed1
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-02-28 09:09:57 +0700

    move navbar script to external file

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,42 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    <script defer>
-        document.addEventListener("DOMContentLoaded", function() {
-            var dropdownToggle = document.getElementById("navbarDropdown");
-            var dropdownMenu = document.querySelector(".dropdown-menu");
-
-            dropdownToggle.addEventListener("click", function() {
-                dropdownMenu.classList.toggle("show");
-            });
-
-            window.addEventListener("click", function(event) {
-                if (!dropdownToggle.contains(event.target)) {
-                    dropdownMenu.classList.remove("show");
-                }
-            });
-        });
-
-        function open_sidebar() {
-            var sidebar = document.getElementById("sidebar");
-            sidebar.classList.toggle("open");
-
-            var navbar = document.querySelector(".navbar");
-            navbar.classList.toggle("sidebar-open");
-
-            toggleSidebar();
-        }
-
-        function close_sidebar() {
-            document.getElementById("sidebar").style.display = "none";
-        }
-
-        function toggleSidebar() {
-            var sidebar = document.getElementById("sidebar");
-            var rabContainer = document.querySelector(".rab-container");
-            rabContainer.classList.toggle("add-margin");
-        }
-    </script>
+    <script src="{{ asset('js/navbar.js') }}"></script>
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css', 'resources/css/navbar.css', 'resources/css/sidebar.css'])
     @livewireStyles
 </head>
