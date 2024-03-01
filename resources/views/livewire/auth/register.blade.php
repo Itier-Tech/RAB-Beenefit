24cd821f91ec7689a268d1b769903dadb009d753
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-03-01 11:48:25 +0700

    Fix language in register to Indonesian

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,14 +1,4 @@
 <div style="background-image: url('{{ asset('images/bg_register.png') }}'); height: 100vh; width: 100%; background-size: cover; display: flex; justify-content: center; align-items: center;">
-    <style>
-        @media (max-width: 768px) {
-        .flex-row-responsive {
-            flex-direction: column;
-        }
-        }
-        .form-control:focus {
-        border-color: #28a745 !important;
-        }
-    </style>
     <div class="card shadow-lg" style="width: 80%; background-color: white; border-radius: 2rem; padding: 2rem; box-sizing: border-box;">
         <div class="text-center" style="margin-bottom: 2rem;">
             <img src="{{ asset('images/logo_beenefit.png') }}" alt="Beenefit Logo" style="width: 120px; margin-bottom: 1rem;" >
@@ -20,15 +10,15 @@
                 <!-- Email input -->
                 <div class="form-group" style="flex: 1;">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" id="email" wire:model="email" style="border-color: #228B22" class="form-control" placeholder="Enter your email" >
+                    <input type="email" id="email" wire:model="email" style="border-color: #228B22" class="form-control" placeholder="Masukkan email" >
                     @error('email')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <!-- Name input -->
                 <div class="form-group" style="flex: 1;">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" id="name" wire:model="full_name" style="border-color: #228B22" class="form-control" placeholder="Enter your name">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" id="name" wire:model="full_name" style="border-color: #228B22" class="form-control" placeholder="Masukkan nama">
                     @error('full_name')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -38,7 +28,7 @@
             <div class="flex-row-responsive" style="display: flex; justify-content: space-between; gap: 1rem;">
                 <!-- Phone input -->
                 <div class="form-group" style="flex: 1;">
-                    <label for="phone" class="form-label">Phone Number</label>
+                    <label for="phone" class="form-label">Nomor telepon</label>
                     <input type="tel" id="phone" wire:model="phone" style="border-color: #228B22" class="form-control" placeholder="08************">
                     @error('phone')
                         <div class="invalid-feedback">{{ $message }}</div>
@@ -47,7 +37,7 @@
                 <!-- Password input -->
                 <div class="form-group" style="flex: 1;">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" id="password" wire:model="password" style="border-color: #228B22" class="form-control" placeholder="Enter your password">
+                    <input type="password" id="password" wire:model="password" style="border-color: #228B22" class="form-control" placeholder="Masukkan password">
                     @error('password')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
