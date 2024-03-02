088ec077b6dc3b3680124cea864f2c526a54be5e
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2024-03-02 12:39:12 +0700

    rab final responsif

--- resources/views/livewire/rab-final.blade.php
+++ resources/views/livewire/rab-final.blade.php
@@ -5,7 +5,7 @@
     @once
         <link href="{{ asset('css/rabFinal.css') }}" rel="stylesheet">
     @endonce
-    <div class="progres-section" style="width: 65rem; height: 150px; background-color: white; border-radius: 20px; align-items: center!important; display: flex; flex-direction: column; justify-content: center;">
+    <div class="progres-section">
         <div class="rab-info d-flex" style="justify-content: space-between; width: 96%;">
             <div class="left">Input RAB</div>
             <div class="right">Final RAB</div>
@@ -14,26 +14,26 @@
             <div style="background-color: #228B22; width: min-content; border-radius: 30px; padding: 4px;">
                 <img src="/images/input-rab.png" alt="Input RAB">
             </div>
-            <div class="progress prg" style="height: 15px; width: 900px;">
-                <div class="progress-bar" role="progressbar" style="width: 100%; background-color: #228B22;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress prg">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <div style="background-color: #228B22; width: min-content; border-radius: 30px; padding: 4px;">
                 <img src="/images/final-rab.png" alt="Final RAB">
             </div>
         </div>
     </div>
-    <div class="m-3" style="display: flex; justify-content: space-between; width: 65rem; align-items: end; padding-top: 15px ;">
+    <div class="m-3 pr-dt">
         <div class="left" style="font-size: 25px; font-weight: 1000;">
         Final RAB
-        @php
+            @php
                 $rab = App\Models\Rab::findOrFail($rab_id);
                 $project = App\Models\Project::findOrFail($rab->project_id);
                 echo $project->project_name;
             @endphp</div>
-        <a class="right" href="#" style="color: #228B22; font-weight: 700; cursor: pointer;">Lihat Detail</a>
+        <a class="right" href="/rab-detail/{{$rab_id}}" style="color: #228B22; font-weight: 700; cursor: pointer;">Lihat Detail</a>
     </div>
-    <div class="download-section" style="padding: 30px; width: 65rem; height: 125px; background-color: white; border-radius: 20px; align-items: center!important; display: flex; justify-content: space-between;">
-        <div class="m-3" style="justify-content: space-between; width: 38rem; align-items: end; font-weight: 700; font-size: 18px;">
+    <div class="download-section">
+        <div class="m-3 ds">
             RAB Proyek
             @php
                 $rab = App\Models\Rab::findOrFail($rab_id);
@@ -42,7 +42,7 @@
             @endphp
             .pdf
         </div>
-        <button class="download-btn" onclick="window.location='/generate-pdf/{{ $rab_id }}'" type="button" style="background-color: #228B22; color: white; border: none; border-radius: 5px; width: 16rem; text-align: left; padding: 10px; padding-top: 15px; padding-bottom: 15px;">Download RAB</button>
+        <button class="download-btn" onclick="window.location='/generate-pdf/{{ $rab_id }}'" type="button">Download RAB</button>
     </div>
     <!-- <div style="padding: 15px; width: 65rem;">
         <div style="font-weight: 600;">Catatan:</div>
